96d2becfd8d7214ddcd6d71c5c15f0823f0bf711
Christina Lin <cristynalin@gmail.com>
2017-04-16 15:10:09 -0700

    Fix various compile errors

--- src/knn.cpp
+++ src/knn.cpp
@@ -11,20 +11,21 @@ kNN::~kNN() {
 }
 
 void kNN::train() {
-    buildMatrix(ratings, false);
-    return;
+    std::cout << "entered train()" << std::endl;
+    std::cout << "ratings size = " << ratings.size() << std::endl;
+    buildMatrix(ratings);
 }
 
 // Calculates Pearson correlation for each item
 // Optimized to not go through entire data set twice
-void kNN::pearson(std::vector<float> x_i, std::vector<float> x_j) {
+float kNN::pearson(std::vector<float> x_i, std::vector<float> x_j) {
     float x_i_ave = 0;
     float x_j_ave = 0;
     int L = 0;
     std::vector<int> index;
 
     for (int i = 0; i < x_i.size(); i++) {
-        if (x_i[i] != NULL && x_j[i] != NULL) {
+        if (x_i[i] != 0 && x_j[i] != 0) {
             x_i_ave = x_i_ave + x_i[i];
             x_j_ave = x_j_ave + x_j[i];
             L++;
@@ -32,13 +33,21 @@ void kNN::pearson(std::vector<float> x_i, std::vector<float> x_j) {
             index.push_back(i);
         }
     }
-    x_i_ave = x_i_ave/L;
-    x_j_ave = x_j_ave/L;
+
+    if (L == 0) {
+        return 0;
+    }
+
+    x_i_ave = x_i_ave / L;
+    x_j_ave = x_j_ave / L;
 
     float nominator = 0;
 
     float denom1 = 0;
     float denom2 = 0;
+
+    float ith_part;
+    float jth_part;
 
     for (int i = 0; i < index.size(); i++) {
         ith_part = x_i[index[i]] - x_i_ave;
@@ -49,28 +58,51 @@ void kNN::pearson(std::vector<float> x_i, std::vector<float> x_j) {
         denom1 = denom1 + (ith_part * ith_part);
         denom2 = denom2 + (jth_part * jth_part);
     }
-    corr = nominator/sqrt(denom1 * denom2);
+    float corr = nominator/sqrt(denom1 * denom2);
 
     return corr;
 }
 
-void kNN::buildMatrix(std::vector<std::vector<float>> train, bool movie) {
-    int N = N_MOVIES;
-    
-    // NEED TO INIT corrMatrix
+void kNN::buildMatrix(std::vector<std::vector<float>> &train) {
+    std::cout << "entered buildMatrix()\n";
+    int N = N_USERS;
 
-    if (!movie) {
-        N = N_USERS;
+    // NEED TO INIT corrMatrix
+    //corrMatrix.clear();
+    //std::cout << "matrix cleared\n";
+    //corrMatrix.resize(N_USERS, std::vector<float>(N_USERS, 0));
+
+    // for (int i = 0; i < N_USERS; i++) {
+    //     std::vector<float> row;
+    //     for (int j = 0; j < N_USERS; j++) {
+    //         row.push_back(0);
+    //     }
+    //     std::cout << "push row " << i << std::endl;
+    //     corrMatrix.push_back(row);
+    // }
+
+    // Initialize corrMatrix with three rows
+    // 0: correlation
+    // 1: user i
+    // 2: user j
+    // where i < j
+    for (int i = 0; i < 3; i++) {
+        std::vector<float> row;
+        corrMatrix.push_back(row);
     }
 
+
+    std::cout << "matrix initialized\n";
     for (int i = 0; i < N - 1; i++) {
         for (int j = 0; j < N; j++) {
-            if (!movie) {
-                corrMatrix[i][j] = pearson(train[i], train[j]);
-            }
-            else {
-                // shouldn't get here; figure out how to optimize
-                std::cout << "cannot build matrix based on movies\n";
+            //std::cout << "Running pearson on " << i << ", " << j << std::endl;
+            //corrMatrix[i][j] = pearson(train[i], train[j]);
+            float corr = pearson(train[i], train[j]);
+            if (corr != 0) {
+                corrMatrix[0].push_back(corr);
+                corrMatrix[1].push_back(i);
+                corrMatrix[2].push_back(j);
+                std::cout << "pushed a nonzero correlation";
             }
         }
     }
@@ -100,7 +132,10 @@ int main(int argc, char **argv) {
     clock_t time2 = clock();
 
     // Train by building correlation matrix
+    std::cout << "Begin training\n";
     knn->train();
+
+    clock_t time3 = clock();
 
     // Predict ratings
     // Load qual data
@@ -115,6 +150,9 @@ int main(int argc, char **argv) {
     std::cout << "Loading took " << ms2 << " ms" << std::endl;
     double total_ms = diffclock(time2, time0);
     std::cout << "Total took " << total_ms << " ms" << std::endl;
+
+    double kNN_ms = diffclock(time3, time0);
+    std::cout << "kNN took " << kNN_ms << " ms" << std::endl;
 
     return 0;
 }
--- src/knn.hpp
+++ src/knn.hpp
@@ -10,8 +10,8 @@ class kNN : public Model {
         void train();
         void predict(int user, int movie);
     private:
-        void pearson(<std::vector<float> x_i, <std::vector<float> x_j);
-        void buildMatrix(std::vector<std::vector<float>> train, bool movie);
+        float pearson(std::vector<float> x_i, std::vector<float> x_j);
+        void buildMatrix(std::vector<std::vector<float>> &train);
         std::vector<std::vector<float>> corrMatrix;
 };
 
