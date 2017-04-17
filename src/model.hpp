92d57c7b391bdd95d1b828e64ab7766ad988804e
Justin Leong <justinleong16@gmail.com>
2017-04-17 00:09:03 -0700

    Fix indentation

--- src/model.hpp
+++ src/model.hpp
@@ -21,9 +21,9 @@
 
 // Returns the differences in ms.
 static double diffclock(clock_t clock1, clock_t clock2) {
-  double diffticks = clock1 - clock2;
-  double diffms = (diffticks) / (CLOCKS_PER_SEC / 1000);
-  return diffms;
+    double diffticks = clock1 - clock2;
+    double diffms = (diffticks) / (CLOCKS_PER_SEC / 1000);
+    return diffms;
 }
 
 class Model {
