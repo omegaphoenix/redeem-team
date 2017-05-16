71ee1fc1870da96be126ed7d6484db11103ca934
Justin Leong <justinleong16@gmail.com>
2017-05-16 02:06:15 -0700

    Add new rbm files

---
+++ src/pure_rbm.cpp
@@ -0,0 +1,94 @@
+#include "pure_rbm.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+RBM::RBM() {
+}
+
+RBM::~RBM() {
+}
+
+void RBM::init() {
+    load("1.dta");
+    clock_t time0 = clock();
+    debugPrint("Initializing...\n");
+
+    // Initial weights
+    for (int j = 0; j < N_MOVIES; ++j) {
+        for (int i = 0; i < TOTAL_FEATURES; ++i) {
+            for (int k = 0; k < SOFTMAX; ++k) {
+                // TODO: Normal Distribution
+                vishid[j][k][i] = 0.02 * randn() - 0.004;
+            }
+        }
+    }
+
+    // Initial biases
+    for (int i = 0; i < TOTAL_FEATURES; ++i) {
+        hidbiases[i] = 0.0;
+    }
+
+    for (int j=0; j < N_MOVIES; ++j) {
+        for (int i = 0; i < SOFTMAX; ++i) {
+            // TODO: Normal Distribution
+            visbiases[j][i] = 0.02 * randn() - 0.004;
+        }
+    }
+    clock_t time1 = clock();
+    float ms1 = diffclock(time1, time0);
+    printf("Initializing took %f ms\n", ms1);
+}
+
+void RBM::train(std::string saveFile) {
+    init();
+
+    // Optimize current feature
+    double nrmse=2., lastRMSE=10.;
+    double prmse = 0, lastPRMSE=0;
+    int loopcount=0;
+    double epsilonW = EPSILONW;
+    double epsilonVB = EPSILONVB;
+    double epsilonHB = EPSILONHB;
+    double momentum = MOMENTUM;
+    ZERO(CDinc);
+    ZERO(visbiasinc);
+    ZERO(hidbiasinc);
+    int tSteps = 1;
+
+    // Iterate through the model while the RMSE is decreasing
+    while (((nrmse < (lastRMSE-E)) || loopcount < 14) && loopcount < 80)  {
+        if ( loopcount >= 10 ) {
+            tSteps = 3 + (loopcount - 10)/5;
+        }
+
+        lastRMSE = nrmse;
+        lastPRMSE = prmse;
+        clock_t t0 = clock();
+        loopcount++;
+        int ntrain = 0;
+        nrmse = 0.0;
+        double s  = 0.0;
+        int n = 0;
+
+        if (loopcount > 5) {
+            momentum = FINAL_MOMENTUM;
+        }
+
+        // CDpos =0, CDneg=0 (matrices)
+        ZERO(CDpos);
+        ZERO(CDneg);
+        ZERO(poshidact);
+        ZERO(neghidact);
+        ZERO(posvisact);
+        ZERO(negvisact);
+        ZERO(moviecount);
+
+    }
+}
+
+float RBM::predict(int n, int i) {
+    return 0;
+}
---
+++ src/pure_rbm.hpp
@@ -0,0 +1,54 @@
+#ifndef PURE_RBM_HPP
+#define PURE_RBM_HPP
+#include "model.hpp"
+
+#define TOTAL_FEATURES  100
+#define SOFTMAX         5
+#define EPSILONW        0.001   // Learning rate for weights
+#define EPSILONVB       0.008   // Learning rate for biases of visible units
+#define EPSILONHB       0.0006  // Learning rate for biases of hidden units
+#define weightcost      0.0001
+#define MOMENTUM        0.8
+#define FINAL_MOMENTUM   0.9
+#define ZERO(v) memset(v,0,sizeof(v))
+#define E  (0.00002) // stop condition
+
+class RBM : public Model {
+
+    public:
+        RBM();
+        ~RBM();
+        void init();
+        void train(std::string saveFile);
+        float predict(int n, int i);
+
+    private:
+        // vishid are the weights.
+        double vishid[N_MOVIES][SOFTMAX][TOTAL_FEATURES];
+        double visbiases[N_MOVIES][SOFTMAX];
+        double hidbiases[TOTAL_FEATURES];
+        double CDpos[N_MOVIES][SOFTMAX][TOTAL_FEATURES];
+        double CDneg[N_MOVIES][SOFTMAX][TOTAL_FEATURES];
+        double CDinc[N_MOVIES][SOFTMAX][TOTAL_FEATURES];
+
+        double poshidprobs[TOTAL_FEATURES];
+        char   poshidstates[TOTAL_FEATURES];
+        char   curposhidstates[TOTAL_FEATURES];
+        double poshidact[TOTAL_FEATURES];
+        double neghidact[TOTAL_FEATURES];
+        double neghidprobs[TOTAL_FEATURES];
+        char   neghidstates[TOTAL_FEATURES];
+        double hidbiasinc[TOTAL_FEATURES];
+
+        double nvp2[N_MOVIES][SOFTMAX];
+        double negvisprobs[N_MOVIES][SOFTMAX];
+        char   negvissoftmax[N_MOVIES];
+        double posvisact[N_MOVIES][SOFTMAX];
+        double negvisact[N_MOVIES][SOFTMAX];
+        double visbiasinc[N_MOVIES][SOFTMAX];
+
+        unsigned int moviercount[SOFTMAX*N_MOVIES];
+        unsigned int moviecount[N_MOVIES];
+
+};
+#endif // PURE_RBM_HPP
