0bea9049e36dbc41bcd6fcc60befe11bc8c97d04
dqu123 <davidqu12345@gmail.com>
2017-05-26 17:41:16 -0700

    Add python script to automate uploading

---
+++ upload.py
@@ -0,0 +1,36 @@
+import argparse
+from bs4 import BeautifulSoup
+import re
+import requests
+import sys
+
+URL = 'http://cs156.caltech.edu/scoreboard'
+
+def run(fname):
+    payload = {'teamid': 'ckbslftg', 'valset': 1}
+    file_ = {'file': (fname, open(fname, 'rb'))}
+    r = requests.post(URL, data=payload, files=file_)
+    return scrape(r.text, fname)
+
+def scrape(html, fname):
+    soup = BeautifulSoup(html, 'html.parser')
+    # 'Your current submission ... '
+    text = soup.find_all('h3')[1].get_text()
+
+    rmse = None
+    regex = re.search('RMSE: (.+?) .*', text)
+    if regex:
+        rmse = float(regex.group(1))
+        sys.stderr.write('{}: {}'.format(fname, regex.group(0)))
+    return rmse
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', metavar='file', type=str, nargs='+')
+    args = parser.parse_args()
+
+    for f in args.files:
+        print ('Uploading {}'.format(f))
+        rmse = run(f)
+        print ('{} rmse: {}'.format(f, rmse))
